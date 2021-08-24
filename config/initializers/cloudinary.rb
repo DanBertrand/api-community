045a0225a981a15c2b3d8136520548b10b5dffb5
DanBertrand <dan.bertrand.hs@gmail.com>
2021-08-24 10:33:16 +0200

    Add and configure Cloudinary for Avatars

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,11 @@
+Cloudinary.config do |config|
+  config.cloud_name = "dbrtrnd"
+  config.api_key =  "335559272452962"
+  config.api_secret =  "eYa7nuapxlrtGsRUM_0nOfupYm0"
+  config.secure = true
+  config.cdn_subdomain = true
+end
+
+
+# CLOUDINARY_KEY ="335559272452962"
+# CLOUDINARY_SECRET="eYa7nuapxlrtGsRUM_0nOfupYm0"
