e4e04dbda62a7df89a891c08008aef72fbfb6712
DanBertrand <dan.bertrand.hs@gmail.com>
2021-09-28 20:02:13 +0200

    modified Community model

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,36 +1,77 @@
 class Community < ApplicationRecord
   has_many :members
-  has_many :users, through: :members
+  has_many :users, through: :community_members
+  has_many :workshops
+  has_many :jobs
   has_one :address, as: :addressable
 
+  # scope :creator, -> { where(role: 'creator') }
+
+  def add_address(params)
+    self.address =
+      Address.create(
+        formatted_address: params[:formatted_address],
+        house_number: params[:house_number],
+        street: params[:street],
+        post_code: params[:post_code],
+        country: params[:country],
+        state: params[:state],
+        city: params[:city],
+        longitude: params[:longitude],
+        latitude: params[:latitude],
+      )
+  end
+
+  def add_creator(user)
+    creator = Member.create(community: self, user: user, role: 'creator')
+    self.members << creator
+  end
 
   def creator
-    member = Member.where(community_id: self.id, role:"creator")[0]
-    user = UserSerializer.new(User.find(member[:user_id])).serializable_hash[:data][:attributes]
-    user[:member_id] = member[:id]
-    return user
+    member = Member.where(community_id: self.id, role: 'creator')[0]
+    if member
+      user =
+        UserSerializer.new(User.find(member[:user_id])).serializable_hash[
+          :data
+        ][
+          :attributes
+        ]
+      user[:member_id] = member[:id]
+      return user
+    end
   end
 
   def basic_members
     members = []
-    Member.where(community_id: self.id, role:"basic").each do |member|
-      user = UserSerializer.new(User.find(member[:user_id])).serializable_hash[:data][:attributes]
-      user[:member_id] = member[:id]
-      members << user
-    end
+    Member
+      .where(community_id: self.id, role: 'basic')
+      .each do |member|
+        user =
+          UserSerializer.new(User.find(member[:user_id])).serializable_hash[
+            :data
+          ][
+            :attributes
+          ]
+        user[:member_id] = member[:id]
+        members << user
+      end
     return members
   end
 
   def moderators_members
     members = []
-    Member.where(community_id: self.id, role:"moderator").each do |member|
-      user = UserSerializer.new(User.find(member[:user_id])).serializable_hash[:data][:attributes]
-      user[:member_id] = member[:id]
-      members << user
-    end
+    Member
+      .where(community_id: self.id, role: 'moderator')
+      .each do |member|
+        user =
+          UserSerializer.new(User.find(member[:user_id])).serializable_hash[
+            :data
+          ][
+            :attributes
+          ]
+        user[:member_id] = member[:id]
+        members << user
+      end
     return members
   end
-
-
-
 end
